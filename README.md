48a34bd64241e4bafe60287b22cfcdd73e2388ec
Osvaldo Jiang <osvaldo@publitar.com>
2019-08-03 22:26:49 -0300

    LID v1

---
+++ README.md
@@ -0,0 +1,119 @@
+# LID
+
+LID (LongID) is a 64-bit number used to identify rows in a database.  
+Made specifically for use with BIGINT/LONGINT in databases. Includes a checksum to verify data.
+
+## Install
+
+```sh
+composer require choval/lid
+```
+
+## Format
+
+It is built using a base 62:
+
+```
+0123456789
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+```
+
+* Except the web format, see <a href="#web">Web</a>.
+
+The checksum is in the first character.
+
+### Examples
+
+```
+NUM: 9223372036854775807
+WEB: 45FE-KXVC-3KT2-6XC7
+LID: 4aZl:8N0y:58M7
+
+NUM: 8057322199735401981
+WEB: 14V7-27T8-8CT6-54X1
+LID: 19Bc:CvJ3:BrCR
+
+NUM: 495653535173419733
+WEB: 8061-03H6-471H-T96H
+LID: 80AC:66ri:lnk9
+
+NUM: 21398
+WEB: 3000-0000-0000-2H9V
+LID: 3000:0000:05z8
+```
+
+
+<a name="web"></a>
+## Web
+
+A different base is used for the web format.
+
+Altough longer in length due to a shorter base, it is easier for dictation (across dialects) and more error friendly for OCR engines.
+
+Alias characters are replaced.
+
+```
+0 - zero - alias: OoDQ
+1 - one - alias: Iil
+2 - two - alias: Zz
+3 - three
+4 - four - alias: AYy
+5 - five - alias: Ss
+6 - six - alias: Gbh
+7 - seven - alias: 
+8 - eight - alias: BRg
+9 - nine - alias: q
+C - charlie
+E - echo
+F - foxtrot
+H - hotel
+J - juliett
+K - kilo
+N - november - alias: Mm
+T - tango - alias: Pp
+V - victor - alias: UuWw
+X - xray
+```
+
+## Usage
+
+```php
+/**
+ * Basic usage
+ */
+$lid = new Lid( 21398 );
+echo $lid->id();
+// 3000:0000:05z8
+echo $lid->web();
+// 3000-0000-0000-2H9V
+echo $lid->number();
+// 21398
+
+/**
+ * The constructor accepts an int, a LID or a Web LID
+ */
+$lid = new Lid( '3000-0000-0000-2H9V' );
+echo $lid->id();
+// 3000:0000:05z8
+echo $lid->web();
+// 3000-0000-0000-2H9V
+echo $lid->number();
+// 21398
+
+/**
+ * Notice how we pass characters not in the base.
+ */
+$lid = new Lid( '3ooo-oOoo-ooo0-zHqU' );
+echo $lid->id();
+// 3000:0000:05z8
+echo $lid->web();
+// 3000-0000-0000-2H9V
+echo $lid->number();
+// 21398
+```
+
+## License
+
+MIT, see LICENSE
+
---
+++ src/Lid.php
@@ -0,0 +1,291 @@
+<?php
+namespace Choval;
+
+class Lid {
+
+
+  /**
+   * Constants
+   */
+  const LID = 'L';
+  const MODE_BASE = 'L';
+  const MODE_WEB = 'W';
+
+
+  /**
+   * Number
+   */
+  protected $number = false;
+  protected $mode = '';
+
+
+
+  /**
+   * Bases
+   */
+  protected static $web_base = '0123456789CEFHJKNTVX';
+  protected static $web_alias = [
+    '0' => ['O','o','D','Q'],
+    '1' => ['I','i','L','l'],
+    '2' => ['Z','z'],
+    '3' => [],
+    '4' => ['A','Y','y'],
+    '5' => ['S','s'],
+    '6' => ['G','b',],
+    '7' => [],
+    '8' => ['B','R','g'],
+    '9' => ['q'],
+    'C' => ['c'],
+    'E' => ['e'],
+    'F' => ['f'],
+    'H' => ['h'],
+    'J' => ['j'],
+    'K' => ['k',],
+    'N' => ['n','M','m'],
+    'T' => ['t','P','p'],
+    'V' => ['v','U','u','W','w'],
+    'X' => ['x'],
+  ];
+  protected static $id_base = '0123456789'.'abcdefghijklmnopqrstuvwxyz'.'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+
+  /**
+   * Constructor
+   */
+  public function __construct($id) {
+    if(is_numeric($id)) {
+      $this->number = $id;
+    } else {
+      $this->number = static::parse($id, $this->mode);
+    }
+    if($this->number === false) {
+      throw new \Exception('Not a valid IdHash');
+    }
+  }
+
+
+
+  /**
+   * To string
+   */
+  public function __toString() {
+    return $this->id();
+  }
+
+
+
+  /**
+   * Returns the number
+   */
+  public function number() {
+    return $this->number;
+  }
+
+
+
+  /**
+   * Returns the ID
+   */
+  public function id() {
+    return static::convert($this->number, static::MODE_BASE);
+  }
+
+
+  
+  /**
+   * Alias of id
+   */
+  public function hid() {
+    return $this->id();
+  }
+
+
+
+  /**
+   * Returns the MODE_WEB version
+   */
+  public function web(bool $checksum=false) {
+    return static::convert($this->number, static::MODE_WEB);
+  }
+
+
+
+  /**
+   * Converts from a number to an IdHash
+   */
+  public static function convert(int $id, string $mode=null) {
+    if(is_null($mode)) {
+      $mode = static::MODE_BASE;
+    }
+    $checksum = static::checksum($id);
+    switch($mode) {
+      case static::MODE_BASE:
+        $max = 11;
+        $sep = ':';
+        $base = static::$id_base;
+        break;
+      case static::MODE_WEB:
+        $max = 16;
+        $sep = '-';
+        $base = static::$web_base;
+        break;
+      default:
+        return false;
+    }
+    $s = static::toBase($id, $base);
+    $s = str_pad($s, $max, '0', STR_PAD_LEFT);
+    if($mode == static::MODE_BASE) {
+      $s = $checksum.$s;
+    } else {
+      $i = substr($s, 0, 1);
+      $s = $checksum.substr($s, 1);
+    }
+    $s = implode($sep, str_split($s, 4));
+    return $s;
+  }
+
+
+
+  /**
+   * Converts from an IdHash to a number
+   */
+  public static function parse(string $id, string &$mode='') {
+    if(is_numeric($id)) {
+      return (int)$id;
+    }
+    $len = strlen($id);
+    $hasColon = strpos($id, ':');
+    $hasMinus = strpos($id, '-');
+    $mode = false;
+    if($len == 14 && $hasColon) {
+      $mode = static::MODE_BASE;
+    }
+    else if($len == 19 && $hasMinus) {
+      $mode = static::MODE_WEB;
+    }
+    else if($hasColon === false && $hasMinus === false) {
+      if($len == 12) {
+        $mode = static::MODE_BASE;
+      }
+      else if($len == 16) {
+        $mode = static::MODE_WEB;
+      }
+    }
+    $id = str_replace(['-',':'],'',$id);
+    $checksum = false;
+    $number = false;
+    switch($mode) {
+      case static::MODE_BASE:
+        $checksum = substr($id, 0, 1);
+        $id = substr($id, 1); 
+        $number = static::toInt($id, static::$id_base);
+        $check = static::checksum($number);
+        if($check != $checksum) {
+          return false;
+        }
+        break;
+      case static::MODE_WEB:
+        foreach(static::$web_alias as $k=>$vs) {
+          $id = str_replace($vs, $k, $id);
+        }
+        $checksum = substr($id, 0, 1);
+        $id = substr($id, 1);
+        $number = static::toInt($id, static::$web_base);
+        $check = static::checksum($number);
+        if($check != $checksum) {
+          return false;
+        }
+        break;
+    }
+    return $number;
+  }
+
+
+
+  /**
+   * Convert to base
+   */
+  public static function toBase(int $number, string $base) : string {
+    return static::convBase($number, '0123456789', $base);
+  }
+
+
+
+  /**
+   * Convert to int
+   */
+  public static function toInt(string $id, string $base) {
+    return static::convBase($id, $base, '0123456789');
+  }
+
+
+
+  /**
+   * Implemented from https://github.com/tdely/luhn-php/blob/master/src/Luhn.php
+   */
+  protected static function algorithm(int $number) : int {
+    $sum = 0;
+    $parity = 1;
+    $raw = (string)$number;
+    $len = strlen($raw);
+    for($i=$len-1;$i>=0;$i--) {
+      $factor = $parity ? 2: 1;
+      $parity = !$parity;
+      $sum += array_sum(str_split($raw[$i] * $factor));
+    }
+    return $sum;
+  }
+
+
+
+  /**
+   * Implemented from https://github.com/tdely/luhn-php/blob/master/src/Luhn.php
+   */
+  public static function checksum(int $number) : int {
+    return ((static::algorithm($number) * 9) % 10);
+  }
+
+
+
+
+  /**
+   * From https://gist.github.com/macik/4758146
+   */
+  public static function convBase($numberInput, $fromBaseInput, $toBaseInput)
+  {
+    if($fromBaseInput == $toBaseInput) {
+      return $numberInput;
+    }
+    $fromBase = str_split($fromBaseInput,1);
+    $toBase = str_split($toBaseInput,1);
+    $number = str_split($numberInput,1);
+    $fromLen=strlen($fromBaseInput);
+    $toLen=strlen($toBaseInput);
+    $numberLen=strlen($numberInput);
+    $retval='';
+    if($toBaseInput == '0123456789') {
+      $retval=0;
+      for($i = 1;$i <= $numberLen; $i++) {
+        $retval = bcadd($retval, bcmul(array_search($number[$i-1], $fromBase),bcpow($fromLen,$numberLen-$i)));
+      }
+      return $retval;
+    }
+    if($fromBaseInput != '0123456789') {
+      $base10=convBase($numberInput, $fromBaseInput, '0123456789');
+    } else {
+      $base10 = $numberInput;
+    }
+    if($base10<strlen($toBaseInput)) {
+      return $toBase[$base10];
+    }
+    while($base10 != '0') {
+      $retval = $toBase[bcmod($base10,$toLen)].$retval;
+      $base10 = bcdiv($base10,$toLen,0);
+    }
+    return $retval;
+  }
+
+
+
+}
